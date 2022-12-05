b3a23bf08552daae7a056b2f5611989c22665876
Francisco Lara <92577198+Phyrov@users.noreply.github.com>
2022-12-05 17:59:29 +0100

    P1

---
+++ P1/fumadores.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cassert>
+#include <thread>
+#include <mutex>
+#include <random> // dispositivos, generadores y distribuciones aleatorias
+#include <chrono> // duraciones (duration), unidades de tiempo
+#include "scd.h"
+
+using namespace std ;
+using namespace scd ;
+
+// numero de fumadores 
+
+const int num_fumadores = 3 ;
+
+Semaphore
+   ingr_disp[num_fumadores] = {0,0,0}, // Indica que ingrediente está disponible
+   mostr_vacio(1);                        // 1 si el mostrador está libre, 0 si hay un ingrediente
+
+//-------------------------------------------------------------------------
+// Función que simula la acción de producir un ingrediente, como un retardo
+// aleatorio de la hebra (devuelve número de ingrediente producido)
+
+int producir_ingrediente()
+{
+   // calcular milisegundos aleatorios de duración de la acción de fumar)
+   chrono::milliseconds duracion_produ( aleatorio<10,100>() );
+
+   // informa de que comienza a producir
+   cout << "Estanquero : empieza a producir ingrediente (" << duracion_produ.count() << " milisegundos)" << endl;
+
+   // espera bloqueada un tiempo igual a ''duracion_produ' milisegundos
+   this_thread::sleep_for( duracion_produ );
+
+   const int num_ingrediente = aleatorio<0,num_fumadores-1>() ;
+
+   // informa de que ha terminado de producir
+   cout << "Estanquero : termina de producir ingrediente " << num_ingrediente << endl;
+
+   return num_ingrediente ;
+}
+
+//----------------------------------------------------------------------
+// función que ejecuta la hebra del estanquero
+
+void funcion_hebra_estanquero(  )
+{
+   int ingrediente;
+   while(true){
+      ingrediente = producir_ingrediente();
+      sem_signal(ingr_disp[ingrediente]);
+      sem_wait(mostr_vacio);
+      cout << "Estanquero : coloca el ingrediente: " << ingrediente << endl;
+   }
+}
+
+//-------------------------------------------------------------------------
+// Función que simula la acción de fumar, como un retardo aleatoria de la hebra
+
+void fumar( int num_fumador )
+{
+
+   // calcular milisegundos aleatorios de duración de la acción de fumar)
+   chrono::milliseconds duracion_fumar( aleatorio<20,200>() );
+
+   // informa de que comienza a fumar
+
+    cout << "Fumador " << num_fumador << "  :"
+          << " empieza a fumar (" << duracion_fumar.count() << " milisegundos)" << endl;
+
+   // espera bloqueada un tiempo igual a ''duracion_fumar' milisegundos
+   this_thread::sleep_for( duracion_fumar );
+
+   // informa de que ha terminado de fumar
+
+    cout << "Fumador " << num_fumador << "  : termina de fumar, comienza espera de ingrediente." << endl;
+
+}
+
+//----------------------------------------------------------------------
+// función que ejecuta la hebra del fumador
+void  funcion_hebra_fumador( int num_fumador )
+{
+   while( true )
+   {
+      sem_wait(ingr_disp[num_fumador]);
+      cout << "Fumador " << num_fumador << " retira: " << num_fumador << endl;
+      sem_signal(mostr_vacio);
+      fumar(num_fumador);
+   }
+}
+
+//----------------------------------------------------------------------
+
+int main()
+{
+   thread hebra_estanquero(funcion_hebra_estanquero);
+   thread hebra_fumador[num_fumadores];
+
+   
+   for (int i=0; i<num_fumadores; i++){
+      hebra_fumador[i] = thread(funcion_hebra_fumador,i);
+   }
+
+   hebra_estanquero.join();
+}
---
+++ P1/prodcons-multiple-FIFO.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <cassert>
+#include <thread>
+#include <mutex>
+#include <random>
+#include "scd.h"
+#include <mutex>
+
+using namespace std ;
+using namespace scd ;
+
+//**********************************************************************
+// Variables globales
+
+const unsigned 
+   num_items = 40 ,   // número de items
+	tam_vec   = 10 ,   // tamaño del buffer
+   num_productoras = 2,
+   num_consumidoras = 4;
+unsigned  
+   cont_prod[num_items] = {0}, // contadores de verificación: para cada dato, número de veces que se ha producido.
+   cont_cons[num_items] = {0}, // contadores de verificación: para cada dato, número de veces que se ha consumido.
+   siguiente_dato       = 0 ;  // siguiente dato a producir en 'producir_dato' (solo se usa ahí)
+
+Semaphore
+   ocupadas(0),      // Inicialmente 0, el valor será: Insertados - extraidos
+   libres(tam_vec);  // Inicialmente tam_vec, el valor será: 
+                     // tam_vector + extraidos - insertados
+
+int 
+   primera_libre = 0,
+   primera_ocupada = 0, 
+   intermedio[tam_vec]; // Vector intermedio
+
+mutex
+   mtx1,
+   mtx2;
+
+//**********************************************************************
+// funciones comunes a las dos soluciones (fifo y lifo)
+//----------------------------------------------------------------------
+
+unsigned producir_dato()
+{
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
+   const unsigned dato_producido = siguiente_dato ;
+   siguiente_dato++ ;
+   cont_prod[dato_producido] ++ ;
+   mtx1.lock();
+   cout << "producido: " << dato_producido << endl << flush ;
+   mtx1.unlock();
+   return dato_producido ;
+}
+//----------------------------------------------------------------------
+
+void consumir_dato( unsigned dato )
+{
+   assert( dato < num_items );
+   cont_cons[dato] ++ ;
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
+   mtx1.lock();
+   cout << "                  consumido: " << dato << endl ;
+   mtx1.unlock();
+}
+
+
+//----------------------------------------------------------------------
+
+void test_contadores()
+{
+   bool ok = true ;
+   cout << "comprobando contadores ...." ;
+   for( unsigned i = 0 ; i < num_items ; i++ )
+   {  if ( cont_prod[i] != 1 )
+      {  cout << "error: valor " << i << " producido " << cont_prod[i] << " veces." << endl ;
+         ok = false ;
+      }
+      if ( cont_cons[i] != 1 )
+      {  cout << "error: valor " << i << " consumido " << cont_cons[i] << " veces" << endl ;
+         ok = false ;
+      }
+   }
+   if (ok)
+      cout << endl << flush << "solución (aparentemente) correcta." << endl << flush ;
+}
+
+//----------------------------------------------------------------------
+
+void  funcion_hebra_productora( int num_hebras )
+{
+   int items_hebra = num_items/num_productoras;
+
+   for( unsigned i = 0 ; i < items_hebra ; i++ )
+   {
+      int dato = producir_dato() ;
+      // FIFO
+      sem_wait(libres);
+      mtx2.lock();
+      intermedio[primera_libre] = dato;
+      primera_libre = (primera_libre+1)%tam_vec;
+      mtx2.unlock();
+      sem_signal(ocupadas);
+   }
+}
+
+//----------------------------------------------------------------------
+
+void funcion_hebra_consumidora( int num_hebras )
+{
+   int items_hebra = num_items/num_consumidoras;
+
+   for( unsigned i = 0 ; i < items_hebra ; i++ )
+   {
+      int dato ;
+      // FIFO
+      sem_wait(ocupadas);
+      mtx2.lock();
+      dato = intermedio[primera_ocupada];
+      primera_ocupada = (primera_ocupada+1)%tam_vec;
+      mtx2.unlock();
+      sem_signal(libres);
+      consumir_dato( dato ) ;
+    }
+}
+//----------------------------------------------------------------------
+
+int main()
+{
+   cout << "-----------------------------------------------------------------" << endl
+        << "Problema de los productores-consumidores (solución FIFO MULTIPLES)." << endl
+        << "------------------------------------------------------------------" << endl
+        << flush ;
+
+   
+   thread hebra_productora[num_productoras];
+   thread hebra_consumidora[num_consumidoras];
+
+   for (int i=0; i<num_productoras; i++){
+      hebra_productora[i] = thread(funcion_hebra_productora,i);
+   }
+
+   for (int i=0; i<num_consumidoras; i++){
+      hebra_consumidora[i] = thread(funcion_hebra_consumidora,i);
+   }
+
+   for (int i=0; i<num_productoras; i++){
+      hebra_productora[i].join();
+   }
+
+   for (int i=0; i<num_consumidoras; i++){
+      hebra_consumidora[i].join();
+   }
+
+
+   test_contadores();
+}
